3e10498012e59b93947697d303f8ce5eeebf637e
2022wb86260-ux <2022wb86260@wilp.bits-pilani.ac.in>
2026-09-24 19:29:21 +0530

    Update greeting message in App.java

--- src/main/java/com/wb86260/App.java
+++ src/main/java/com/wb86260/App.java
@@ -2,7 +2,7 @@ package com.wb86260;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello from wb86260 Web Dev Project - Continuous Integration!");
+        System.out.println("Hello from wb86260 Web Dev Project - devops lab modified!");
     }
 
     public static int add(int a, int b) {
